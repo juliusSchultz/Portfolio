14af3d6b714b7ac334443f1de60014a685298418
julius.schultz <julius.schultz@diva-e.com>
2024-01-20 10:32:11 +0100

    fix Footer stylings

--- components/organisms/Footer/Footer.vue
+++ components/organisms/Footer/Footer.vue
@@ -1,12 +1,13 @@
 <template>
-  <footer>
+  <footer class="footer">
     <nav>
       <div class="nav-links-container">
-        <ul class="nav-links">
-          <li><a href="#about">About</a></li>
-          <li><a href="#experience">Experience</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+        <ul class="footer__links">
+          <li v-for="link in links">
+            <NuxtLink class="footer__link" :to="link.href">
+              {{link.label}}
+            </NuxtLink>
+          </li>
         </ul>
       </div>
     </nav>
@@ -14,15 +15,59 @@
   </footer>
 </template>
 
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: 'Footer',
+  setup(){
+    const links = [
+      {href: '/about', label: 'About'},
+      {href: '/experience', label: 'Experience'},
+      {href: '/projects', label: 'Projects'},
+      {href: '/contact', label: 'Contact'} ]
+
+    return {
+      links
+    }
+
+  }
+})
+</script>
+
 <style>
-footer {
+.footer {
   height: 26vh;
   margin: 0 1rem;
   background-color: #525252;
   color: white;
+
+  &__links {
+    @apply flex;
+    justify-content: center;
+    @apply py-6;
+    gap: 2rem;
+    list-style: none;
+    font-size: 1.5rem;
+  }
+
+  &__link {
+    text-decoration: none;
+    text-decoration-color: white;
+    @apply font-sans;
+  }
+
+  &__link:hover {
+    color: grey;
+    text-decoration: underline;
+    text-underline-offset: 1rem;
+    text-decoration-color: rgb(181, 181, 181);
+  }
 }
 
 footer p {
   text-align: center;
 }
 </style>
+<script setup lang="ts">
+</script>

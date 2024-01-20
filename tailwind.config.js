4f70b2895b9d03f4cb4452476e163a5207b64fc6
julius.schultz <julius.schultz@diva-e.com>
2024-01-20 11:02:02 +0100

    extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,31 @@ export default {
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
     },
+    fontSize: {
+      xs: '.75rem', // 12px
+      sm: '.875rem', // 14px
+      base: '1rem', // 16px
+      lg: '1.25rem', // 20px
+      xl: '1.5rem', // 24px
+      '2xl': '1.75rem', // 28px
+      '3xl': '2rem', // 32px
+      '4xl': '2.25rem', // 36px
+      '5xl': '3rem', // 48px
+      '6xl': '3.5rem', // 56px
+    },
+    lineHeight: {
+      3: '0.75rem', // 12px
+      4: '1rem', // 16px
+      5: '1.25rem', // 20px
+      6: '1.5rem', // 24px
+      7: '1.75rem', // 28px
+      8: '2rem', // 32px
+      9: '2.25rem', // 36px
+      10: '2.5rem', // 40px
+      11: '2.75rem', // 44px
+      14: '3.5rem', // 56px
+      16: '4rem', // 64px
+    },
     screens: {
       xs: 0,
       sm: '576px',

87fcea5626a4a8937f52465c032accb60b6a7a79
julius.schultz <julius.schultz@diva-e.com>
2024-02-06 22:37:05 +0100

    work on projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,59 +1,90 @@
 <template>
-  <section id="projects">
-    <p class="section__text__p1">Browse My Recent</p>
-    <h1 class="title">Projects</h1>
-    <div class="experience-details-container">
-      <div class="about-containers">
-        <div
-          class="details-container color-container"
-          onclick="location.href='https://webportal.pfeiffer-vacuum.com/global/de'"
-        >
-          <div class="article-container">
-            <NuxtImg
-              src="pfeiffer-logo.jpeg"
-              alt="Project 1"
-              class="project-img"
-            />
-          </div>
-          <h2 class="experience-sub-title project-title">Pfeiffer Vacuum</h2>
-          <p>
+  <section class="projects">
+    <SectionHeadline line1="Browse My Recent" line2="Projects" />
+    <div class="projects__wrapper">
+      <div class="projects__unit">
+        <NuxtImg
+          class="projects__picture"
+          src="pfeiffer-logo.jpeg"
+          alt="Julius Schultz profile picture"
+        />
+        <div class="projects__text">
+          <h3 class="projects__project-headline">Pfeiffer Vacuum</h3>
+          <p class="projects__project-description">
             Development of an online shop at diva-e on behalf of Pfeiffer Vacuum
           </p>
-          <br />
-          <div class="btn-container">
-            <button
-              class="btn btn-color-2 project-btn"
-              onclick="location.href='https://webportal.pfeiffer-vacuum.com/global/de'"
-            >
-              Website
-            </button>
-          </div>
+          <button
+            class="btn btn-color-2"
+            onclick="location.href='https://webportal.pfeiffer-vacuum.com/global/de'"
+          >
+            Website
+          </button>
         </div>
-        <div class="details-container color-container">
-          <div class="article-container">
-            <NuxtImg
-              src="bloomreach.jpeg"
-              alt="Project 2"
-              class="project-img"
-            />
-          </div>
-          <h2 class="experience-sub-title project-title">
-            Bloomreach Connector
-          </h2>
-          <p>Creation of a Connector to Bloomreach for Nuxt.js projects</p>
-          <br />
-          <div class="btn-container">
-            <button
-              class="btn btn-color-2 project-btn"
-              onclick="location.href='https://www.npmjs.com/package/bloomreach-with-nuxt'"
-            >
-              npm package
-            </button>
-          </div>
+      </div>
+      <div class="projects__unit">
+        <NuxtImg
+          class="projects__picture"
+          src="bloomreach.jpeg"
+          alt="Julius Schultz profile picture"
+        />
+        <div class="projects__text">
+          <h3 class="projects__project-headline">Bloomreach Connector</h3>
+          <p class="projects__project-description">
+            Creation of a Connector for Nuxt.js projects to the CMS of npm
+            package
+          </p>
+          <button
+            class="btn"
+            onclick="location.href='https://www.npmjs.com/package/bloomreach-with-nuxt'"
+          >
+            npm package
+          </button>
         </div>
-        <div class="details-container-placeholder"></div>
       </div>
     </div>
   </section>
 </template>
+
 <script setup lang="ts"></script>
+
+<style>
+.projects {
+  @apply text-grey-16;
+
+  &__wrapper {
+    @apply flex flex-col;
+    @apply justify-center;
+    @apply gap-x-4;
+
+    @screen md {
+      @apply flex-row;
+    }
+  }
+
+  &__unit {
+    padding: 20px;
+    border-radius: 2rem;
+    border: rgb(53, 53, 53) 0.1rem solid;
+    border-color: rgb(163, 163, 163);
+    max-width: 450px;
+    text-align: center;
+  }
+
+  &__picture {
+    @apply w-[300px] h-[300px];
+    border-radius: 2rem;
+    @apply pb-6;
+  }
+
+  &__project-headline {
+    @apply text-xl;
+    @apply font-bold;
+    @apply pb-6;
+  }
+
+  &__project-description {
+    @apply text-grey-32;
+    @apply pb-6;
+  }
+}
+</style>

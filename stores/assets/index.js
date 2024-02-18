1961fb2e80343cc04fbbf85e62058494c5d90205
julius.schultz <julius.schultz@diva-e.com>
2024-02-18 16:04:20 +0100

    update vue, update nuxt

--- stores/assets/index.js
+++ stores/assets/index.js
@@ -2,12 +2,15 @@ import { defineStore } from 'pinia'
 
 export const useAssetsStore = defineStore('assets', () => {
   const assetIcons = Object.fromEntries(
-    Object.entries(import.meta.glob('~/assets/svg/*.svg', { as: 'raw' })).map(
-      ([key, value]) => {
-        const filename = key.split('/').pop().split('.').shift()
-        return [filename, value]
-      }
-    )
+    Object.entries(
+      import.meta.glob('~/assets/svg/*.svg', {
+        query: '?raw',
+        import: 'default',
+      })
+    ).map(([key, value]) => {
+      const filename = key.split('/').pop().split('.').shift()
+      return [filename, value]
+    })
   )
 
   const getSvgByName = (name) => assetIcons[name]?.()

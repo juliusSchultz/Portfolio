de2a2923e90dc9fe5b42e9feb24088477e1fc42a
julius.schultz <julius.schultz@diva-e.com>
2024-03-08 19:38:38 +0100

    fix semantic structuring of headlines

--- components/atoms/SectionHeadline/SectionHeadline.vue
+++ components/atoms/SectionHeadline/SectionHeadline.vue
@@ -1,8 +1,8 @@
 <template>
-  <h1 class="section-headline">
+  <div class="section-headline">
     <h6 class="section-headline__headline--1">{{ line1 }}</h6>
     <h1 class="section-headline__headline--2">{{ line2 }}</h1>
-  </h1>
+  </div>
 </template>
 <script>
 import { defineComponent } from 'vue'

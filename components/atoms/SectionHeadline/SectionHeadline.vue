42c788f20e949a6ef09d552d5b390d0a62e6693c
julius.schultz <julius.schultz@diva-e.com>
2024-03-08 19:42:42 +0100

    change h6 to h4

--- components/atoms/SectionHeadline/SectionHeadline.vue
+++ components/atoms/SectionHeadline/SectionHeadline.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="section-headline">
-    <h6 class="section-headline__headline--1">{{ line1 }}</h6>
+    <h4 class="section-headline__headline--1">{{ line1 }}</h4>
     <h1 class="section-headline__headline--2">{{ line2 }}</h1>
   </div>
 </template>
